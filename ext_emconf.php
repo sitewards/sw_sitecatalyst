90fcb5fa1018aabd1c1a857729c47e67c0d81ca0
airbone42 <tobias.zander@sitewards.com>
2013-09-05 14:01:22 +0200

    updated version information

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "sw_sitecatalyst".
  *
- * Auto generated 02-09-2013 15:08
+ * Auto generated 05-09-2013 14:01
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,16 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '2.0.4',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"6eae";s:14:"ext_tables.php";s:4:"cdda";s:24:"ext_typoscript_setup.txt";s:4:"690d";s:16:"locallang_db.xml";s:4:"29ea";s:10:"README.txt";s:4:"8eaf";s:14:"doc/manual.sxw";s:4:"5cc2";s:35:"pi1/class.tx_swsitecatalyst_pi1.php";s:4:"c386";s:27:"templates/sitecatalyst.html";s:4:"3175";}',
+	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"06b9";s:14:"ext_tables.php";s:4:"cdda";s:24:"ext_typoscript_setup.txt";s:4:"1745";s:16:"locallang_db.xml";s:4:"29ea";s:10:"README.txt";s:4:"8eaf";s:14:"doc/manual.sxw";s:4:"5cc2";s:35:"pi1/class.tx_swsitecatalyst_pi1.php";s:4:"01e9";s:27:"templates/sitecatalyst.html";s:4:"449c";}',
 	'suggests' => array(
 	),
 );
